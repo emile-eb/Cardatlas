87f683bf7cd7783112c64bd4035735e2cdd88fb0
emile-eb <cheickhemilebasse@gmail.com>
2026-03-29 16:50:11 -0400

    sds

--- app.config.ts
+++ app.config.ts
@@ -58,7 +58,7 @@ const config = {
     supportsTablet: false,
     bundleIdentifier,
     // Keep this ahead of the latest TestFlight build.
-    buildNumber: "45"
+    buildNumber: "46"
   },
   android: {
     package: bundleIdentifier,
